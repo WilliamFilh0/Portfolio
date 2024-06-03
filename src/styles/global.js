0454a33ae246cd778c78d4d324a39bd5b2696000
WilliamFilh0 <williamfilho8908@gmail.com>
2024-06-03 10:31:54 -0300

    feat: Adding styling to the scrollbar

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,21 +5,30 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    
+  ::-webkit-scrollbar {
+    width: 8px;
+    background-color: rgba(53, 53, 55, 0.9);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #a9a9a9;
+  }
+
+  
+
   }
 
   body{
     min-height: 100vh;
 
    //cor que estava antes 
-   background-color: #242426;
+   /* background-color: #242426; */
 
    //cor um pouco mias clara
-   background-color: #28282A;
+   /* background-color: #28282A; */
 
-   background-color: rgba(45, 45, 47, 1); /* Um pouco mais clara */
-
-    
-
- 
+   background-color:  rgba(45, 45, 47, 1);/* Um pouco mais clara */
   }
 `;
